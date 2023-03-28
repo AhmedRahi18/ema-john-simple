53697a7458c26b6463f2aaf97e12cd3303eb4bfe
AhmedRahi18 <71ahmedrahi@gmail.com>
2023-03-28 12:39:20 +0600

    added cart component and calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -12,7 +13,7 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
     
-    const handleAddToCart = (product) => {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    const handleAddToCart = (product) => {
         const newCart = [...cart,product]
         setCart(newCart)
     }
@@ -29,8 +30,9 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <Cart
+                cart={cart} 
+                ></Cart>
             </div>
         </div>
     );
