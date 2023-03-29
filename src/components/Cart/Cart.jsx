f1fb821d90a25710c9e8368e68017d24d6ffc8f8
AhmedRahi18 <71ahmedrahi@gmail.com>
2023-03-29 15:15:38 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./Cart.css";
 
-const Cart = ({cart}) => {
-    // const cart = props.cart  ----> option-1
-    // const {cart} = props  ----> option-2
-    // option-3 --> props er modhe direct {cart} boshai deya
-  
-    let totalPrice = 0;
-    let totalShipping = 0;
-    for(const product of cart){
-      console.log(cart)
-        totalPrice = totalPrice + product.price
-        totalShipping = totalShipping + product.shipping
-    }
-    const tax = totalPrice*7/100
+const Cart = ({ cart }) => {
+  // const cart = props.cart  ----> option-1
+  // const {cart} = props  ----> option-2
+  // option-3 --> props er modhe direct {cart} boshai deya
 
-    const grandTotal = totalPrice+totalShipping+tax
+  let totalPrice = 0;
+  let totalShipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    if(product.quantity === 0){
+      product.quantity = 1
+    }
+    // product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
+  }
+  const tax = (totalPrice * 7) / 100;
+
+  const grandTotal = totalPrice + totalShipping + tax;
 
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
